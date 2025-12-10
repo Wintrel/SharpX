ec63a05814ddf135ee10e50d017184c9608f30e4
yamokopatel <cvetkovsdeniss4@gmail.com>
2025-12-10 10:40:35 +0200

    Structure extended
    Added (empty) blocks for properties, addedbuttons for figure creation, wided side bars.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -13,11 +13,46 @@ const geistMono = Geist_Mono({
 
 export default function Reductor(){
 	return(
-		<div id="body" style={{display: "grid", gridTemplateColumns: "192px 1fr 192px"}}>
+		<div id="body" style={{display: "grid", gridTemplateColumns: "320px 1fr 320px"}}>
 			<div id="left" style={{gridColumn: "1", display: "grid", gridTemplateRows: "1fr 1fr", borderStyle: "solid", borderColor: "black", borderWidth: "2px"}}>
 				<div id="blocks" style={{gridRow: "1", borderBottomStyle: "solid", borderColor: "black", borderWidth: "1px"}}>
 					<h3>Figures</h3>
-
+					<table>
+						<tr>
+							<td>
+								<button id="arrowButton">
+									I <!-- 'Seit j'ab'ut att'els ar fig'uru -->
+								</button>
+							</td>
+							<td>
+								<button id="squareButton">
+									П <!-- 'Seit j'ab'ut att'els ar fig'uru -->
+								</button>
+							</td>
+							<td>
+								<button id="circleButton">
+									O <!-- 'Seit j'ab'ut att'els ar fig'uru -->
+								</button>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<button id="treangleButton">
+									^ <!-- 'Seit j'ab'ut att'els ar fig'uru -->
+								</button>
+							</td>
+							<td>
+								<button id="pentagonButton">
+									* <!-- 'Seit j'ab'ut att'els ar fig'uru -->
+								</button>
+							</td>
+							<td>
+								<button id="hexagonButton">
+									Ж <!-- 'Seit j'ab'ut att'els ar fig'uru -->
+								</button>
+							</td>
+						</tr>
+					</table>
 				</div>
 				<div id="hierarchy" style={{gridRow: "2", borderTopStyle: "solid", borderColor: "black", borderWidth: "1px"}}>
 					<h3>Figure List</h3>
@@ -34,7 +69,24 @@ export default function Reductor(){
 			</div>
 			<div id="right" style={{gridColumn: "3", borderStyle: "solid", borderColor: "black", borderWidth: "2px"}}>
 				<h3>Properties</h3>
+				<div id="arrowProp" class="prop" style={{display: "none"}}>
 
+				</div>
+				<div id="squareProp" class="prop" style={{display: "none"}}>
+
+				</div>
+				<div id="treangleProp" class="prop" style={{display: "none"}}>
+
+				</div>
+				<div id="pentaProp" class="prop" style={{display: "none"}}>
+
+				</div>
+				<div id="hexaProp" class="prop" style={{display: "none"}}>
+
+				</div>
+				<div id="circleProp" class="prop" style={{display: "none"}}>
+
+				</div>
 			</div>
 		</div>
 	)
